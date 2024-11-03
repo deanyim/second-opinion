d35cf8c1b1f45e1359d553e0f28f777a225aacbc
Dean Yim <dean.d.yim@gmail.com>
2024-11-03 12:09:56 -0500

    fixed some api crap

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,12 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import superjson from 'superjson';
 import type { AppRouter } from '@/server/api/root';
 
-export const api = createTRPCProxyClient<AppRouter>({
+export const api = createTRPCClient<AppRouter>({
     links: [
         httpBatchLink({
             url: '/api/trpc',
+            transformer: superjson,
         }),
     ],
 }); 
